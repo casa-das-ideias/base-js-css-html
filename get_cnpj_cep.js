6094bc454cc8234231cb50c505032de6fe01de06
Gladson <gladson@immensa.page>
2020-10-01 03:10:52 -0400

    refactor: update and rename cep.js to get_cnpj_cep.js

---
+++ get_cnpj_cep.js
@@ -0,0 +1,37 @@
+var id_cnpj = document.getElementById("id_cnpj");
+id_cnpj.addEventListener("blur", (e) => {
+    //e.value = "";
+    //e.target.style.backgroundColor = "yellow";
+    //console.log(id_cnpj.value);
+    consultaCNPJ(id_cnpj.value).then(
+        (json) => {
+            //console.log(json);
+            //console.log(json.bairro);
+            document.getElementById("id_name").value = json.nome;
+            document.getElementById("id_name_fantasy").value = json.fantasia;
+            document.getElementById("id_company_size").value = json.porte;
+            document.getElementById("id_company_legal_nature").value =
+                json.natureza_juridica;
+            document.getElementById("id_company_situation").value = json.situacao;
+        },
+        (erro) => {
+            console.log("ERRO:", erro);
+        }
+    );
+});
+var id_address_cep = document.getElementById("id_address_cep");
+id_address_cep.addEventListener("blur", (e) => {
+
+    consultaCEP(id_address_cep.value).then(
+        (json) => {
+            //console.log(json) 
+            document.getElementById("id_address").value = json.logradouro;
+            document.getElementById("id_address_neighborhood").value = json.bairro;
+            document.getElementById("id_address_neighborhood").value = json.bairro;
+            document.getElementById("id_address_number").focus();
+        },
+        (erro) => {
+            console.log("ERRO:", erro);
+        }
+    );
+});
