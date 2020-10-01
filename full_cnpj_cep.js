e97db60af935897cec0471b7e754d0f2c028d469
Gladson <gladson@immensa.page>
2020-10-01 03:59:51 -0400

    refactor: full_cnpj_cep.js

--- full_cnpj_cep.js
+++ full_cnpj_cep.js
@@ -1,6 +1,74 @@
-function jsonp(e,o){const t="jsonp_"+Math.random().toString(36).substr(2,5);return new Promise(function(n,d){let i=document.createElement("script"),c=setTimeout(()=>{d("Tempo limite atingido"),document.body.removeChild(i)},o);window[t]=(e=>{clearTimeout(c),n(e),document.body.removeChild(i),delete window[t]}),i.src=e+"?callback="+encodeURI(t),document.body.appendChild(i)})}
+/**
+ * Implementação da requisição na web
+ */
 
-function consultaCNPJ(e){return e=e.replace(/\D/g,""),jsonp("https://www.receitaws.com.br/v1/cnpj/"+encodeURI(e),6e4).then(e=>"ERROR"===e.status?Promise.reject(e.message):Promise.resolve(e))}function consultaCEP(e){return 8!==(e=e.replace(/\D/g,"")).length?Promise.reject("CEP inválido"):jsonp("https://viacep.com.br/ws/"+encodeURI(e)+"/json/",3e4).then(e=>!0===e.erro?Promise.reject("CEP não encontrado"):Promise.resolve(e))}
+function jsonp(url, timeout) {
+  // Gera um nome aleatório para a função de callback
+  const func = 'jsonp_' + Math.random().toString(36).substr(2, 5);
+
+  return new Promise(function(resolve, reject) {
+    // Cria um script
+    let script = document.createElement('script');
+
+    // Cria um timer para controlar o tempo limite
+    let timer = setTimeout(() => {
+      reject('Tempo limite atingido');
+      document.body.removeChild(script);
+    }, timeout);
+
+    // Cria a função de callback
+    window[func] = (json) => {
+      clearTimeout(timer);
+      resolve(json);
+      document.body.removeChild(script);
+      delete window[func];
+    };
+
+    // Adiciona o script na página para inicializar a solicitação
+    script.src = url + '?callback=' + encodeURI(func);
+    document.body.appendChild(script);
+  });
+}
+
+/**
+ * Consulta um CNPJ
+ */
+function consultaCNPJ(cnpj) {
+  // Limpa o CNPJ para conter somente numeros, removendo traços e pontos
+  cnpj = cnpj.replace(/\D/g, '');
+
+  // Consulta o CNPJ na ReceitaWS com 60 segundos de tempo limite
+  return jsonp('https://www.receitaws.com.br/v1/cnpj/' + encodeURI(cnpj), 60000)
+    .then((json) => {
+      if (json['status'] === 'ERROR') {
+        return Promise.reject(json['message']);
+      } else {
+        return Promise.resolve(json);
+      }
+    });
+}
+
+/**
+ * Consulta um CEP
+ */
+function consultaCEP(cep) {
+  // Limpa o CEP para conter somente numeros, removendo traços e pontos
+  cep = cep.replace(/\D/g, '');
+
+  // Como a API retorna 404 com CEPs com tamanhos inválidos
+  // Iremos validar antes para não ter que esperar o tempo limite do JSONP
+  if (cep.length !== 8) return Promise.reject('CEP inválido');
+
+  // Consulta o CEP na ViaCEP com 30 segundos de tempo limite
+  return jsonp('https://viacep.com.br/ws/' + encodeURI(cep) + '/json/', 30000)
+    .then((json) => {
+      if (json['erro'] === true) {
+        return Promise.reject('CEP não encontrado');
+      } else {
+        return Promise.resolve(json);
+      }
+    });
+}
 
 var id_cnpj = document.getElementById("id_cnpj");
 if (id_cnpj != null) {
